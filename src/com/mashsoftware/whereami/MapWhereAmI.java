97ee77b33e42b0a75d2615cbcb05b758cfa91d43
Neil Frow <neil@neil-desktop.(none)>
2011-05-24 20:36:41 +0100

    Another attemp at getting useful overlay displayed

--- src/com/mashsoftware/whereami/MapWhereAmI.java
+++ src/com/mashsoftware/whereami/MapWhereAmI.java
@@ -19,19 +19,24 @@ public class MapWhereAmI extends MapActivity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
+		String context = Context.LOCATION_SERVICE;
 		setContentView(R.layout.main);
 		MapView myMapView = (MapView) findViewById(R.id.myMapView);
+
+		MyItemizedOverlay myItemizedOverlay = new MyItemizedOverlay(this.getResources().getDrawable(R.drawable.androidmarker),
+				getApplicationContext());
 		mapController = myMapView.getController();
 
 		myMapView.setSatellite(true);
 		myMapView.setStreetView(true);
 		myMapView.displayZoomControls(false);
-
+		myMapView.getOverlays().add(myItemizedOverlay);
 		mapController.setZoom(17);
-		
+
 		LocationManager locationManager;
-		String context = Context.LOCATION_SERVICE;
+
 		locationManager = (LocationManager) getSystemService(context);
 
 		Criteria criteria = new Criteria();
@@ -40,7 +45,7 @@ public class MapWhereAmI extends MapActivity {
 		criteria.setBearingRequired(false);
 		criteria.setCostAllowed(false);
 		criteria.setPowerRequirement(Criteria.POWER_LOW);
-		
+
 		String provider = locationManager.getBestProvider(criteria, true);
 		Location location = locationManager.getLastKnownLocation(provider);
 		updateWithNewLocation(location);
@@ -52,50 +57,49 @@ public class MapWhereAmI extends MapActivity {
 
 		return false;
 	}
-	
+
 	private void updateWithNewLocation(Location location) {
 		StringBuilder latlongString;
 		TextView myLocationText;
 		myLocationText = (TextView) findViewById(R.id.myLocationText);
 		latlongString = new StringBuilder();
 		if (location != null) {
-			Double geoLat = location.getLatitude()*1E6;
-			Double geoLong = location.getLongitude()*1E6;
+			Double geoLat = location.getLatitude() * 1E6;
+			Double geoLong = location.getLongitude() * 1E6;
 			GeoPoint point = new GeoPoint(geoLat.intValue(), geoLong.intValue());
 			mapController.animateTo(point);
 			double lat = location.getLatitude();
 			double lng = location.getLongitude();
-			latlongString.append("Lat:").append(lat).append("\nLong:")
-					.append(lng);
+			latlongString.append("Lat:").append(lat).append("\nLong:").append(lng);
 		} else {
 			latlongString.append("No location found");
 		}
 		myLocationText.setText(latlongString.toString());
 	}
-	
+
 	private final LocationListener locationListener = new LocationListener() {
-		
+
 		@Override
 		public void onStatusChanged(String provider, int status, Bundle extras) {
-			
+
 		}
-		
+
 		@Override
 		public void onProviderEnabled(String provider) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 		@Override
 		public void onProviderDisabled(String provider) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 		@Override
 		public void onLocationChanged(Location location) {
 			updateWithNewLocation(location);
-			
+
 		}
 	};
 }
